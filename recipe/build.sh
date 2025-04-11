46a72f81bdc0dc620db53db8293c934ccf2c743d
Antônio Camargo <apcamargo@users.noreply.github.com>
2025-04-11 14:05:35 -0700

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,7 +7,8 @@ export CARGO_PROFILE_RELEASE_LTO=fat
 
 if [[ "${build_platform}" == linux-* ]]; then
     export LIBCLANG_PATH="$BUILD_PREFIX/lib"
-    export INCLUDE="$BUILD_PREFIX/include"
+    export BZIP3_LIB_DIR="$BUILD_PREFIX/lib"
+    export BZIP3_INCLUDE_DIR="$BUILD_PREFIX/include"
 fi
 
 # build binary with Cargo
