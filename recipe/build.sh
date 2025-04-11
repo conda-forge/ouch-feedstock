ef6208b43eaf4d4f6d04f95e966ee06be22cb0dc
Antônio Camargo <apcamargo@users.noreply.github.com>
2025-04-11 15:21:58 -0700

    Change build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,11 @@ set -o xtrace -o nounset -o pipefail -o errexit
 export CARGO_PROFILE_RELEASE_STRIP=symbols
 export CARGO_PROFILE_RELEASE_LTO=fat
 
+if [[ "${build_platform}" == linux-* ]]; then
+    export LIBCLANG_PATH="$BUILD_PREFIX/lib"
+fi
+
+# build binary with Cargo
 cargo install --no-track --locked --root "$PREFIX" --path .
 
 cargo-bundle-licenses \
