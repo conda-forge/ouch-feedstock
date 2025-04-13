088d4b83d77df00eda484cf9bfec620e4d94ddea
Antônio Camargo <apcamargo@users.noreply.github.com>
2025-04-12 23:45:08 -0700

    Bump `ouch` to 0.6.0 (#5)
    * Bump `ouch` to 0.6.0
    
    * MNT: Re-rendered with conda-build 25.3.2, conda-smithy 3.47.1, and conda-forge-pinning 2025.04.11.15.16.52
    
    * Add `{{ stdlib("c") }}`
    
    * Use GCC again
    
    * Use GCC again
    
    * MNT: Re-rendered with conda-build 25.3.2, conda-smithy 3.47.1, and conda-forge-pinning 2025.04.11.15.16.52
    
    * Add `libclang` dependency
    
    * Set `LIBCLANG_PATH`
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    * MNT: Re-rendered with conda-build 25.3.2, conda-smithy 3.47.1, and conda-forge-pinning 2025.04.11.15.16.52
    
    * Update compilers
    
    * MNT: Re-rendered with conda-build 25.3.2, conda-smithy 3.47.1, and conda-forge-pinning 2025.04.11.15.16.52
    
    * Change build.sh
    
    * Change build.sh
    
    * Update meta.yaml
    
    * Update meta.yaml
    
    * Add tests
    
    ---------
    
    Co-authored-by: conda-forge-webservices[bot] <91080706+conda-forge-webservices[bot]@users.noreply.github.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
+set -o xtrace -o nounset -o pipefail -o errexit
+
+export CARGO_PROFILE_RELEASE_STRIP=symbols
+export CARGO_PROFILE_RELEASE_LTO=fat
+
+if [[ "${build_platform}" == linux-* ]]; then
+    export LIBCLANG_PATH="$BUILD_PREFIX/lib"
+fi
+
+# build binary with Cargo
+cargo install --no-track --locked --root "$PREFIX" --path .
+
 cargo-bundle-licenses \
     --format yaml \
     --output THIRDPARTY.yml
-cargo install --locked --bins --root "$PREFIX" --path .
-"$STRIP" "$PREFIX/bin/ouch"
-rm -f "${PREFIX}/.crates.toml"
-rm -f "${PREFIX}/.crates2.json"
