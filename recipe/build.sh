830435d0ad930cc8e2b67256be1bd0f320820013
Antônio Camargo <apcamargo@users.noreply.github.com>
2026-06-11 04:31:47 -0700

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,10 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 export CARGO_PROFILE_RELEASE_STRIP=symbols
 export CARGO_PROFILE_RELEASE_LTO=fat
+export CC_aarch64_unknown_linux_gnu="${CC}"
+export CXX_aarch64_unknown_linux_gnu="${CXX}"
+export AR_aarch64_unknown_linux_gnu="${AR}"
+export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER="${CC}"
 
 if [[ "${build_platform}" == linux-* ]]; then
     export LIBCLANG_PATH="$BUILD_PREFIX/lib"
