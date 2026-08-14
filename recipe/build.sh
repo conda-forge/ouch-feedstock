c5c9789ed8731086ca4512c6d24c22d0235a0dbe
Antônio Camargo <apcamargo@users.noreply.github.com>
2026-08-14 19:54:44 -0300

    Bump to v0.8.1

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,16 +4,22 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 export CARGO_PROFILE_RELEASE_STRIP=symbols
 export CARGO_PROFILE_RELEASE_LTO=fat
-export CC_aarch64_unknown_linux_gnu="${CC}"
-export CXX_aarch64_unknown_linux_gnu="${CXX}"
-export AR_aarch64_unknown_linux_gnu="${AR}"
-export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER="${CC}"
 
 if [[ "${build_platform}" == linux-* ]]; then
     export LIBCLANG_PATH="$BUILD_PREFIX/lib"
+
+    export CC_x86_64_unknown_linux_gnu="${CC}"
+    export CXX_x86_64_unknown_linux_gnu="${CXX}"
+    export AR_x86_64_unknown_linux_gnu="${AR}"
+    export CXXFLAGS_x86_64_unknown_linux_gnu="${CXXFLAGS}"
+
+    export CC_aarch64_unknown_linux_gnu="${CC}"
+    export CXX_aarch64_unknown_linux_gnu="${CXX}"
+    export AR_aarch64_unknown_linux_gnu="${AR}"
+    export CXXFLAGS_aarch64_unknown_linux_gnu="${CXXFLAGS}"
 fi
 
-# build binary with Cargo
+# Build binary with cargo-auditable
 cargo auditable install --no-track --locked --root "$PREFIX" --path .
 
 cargo-bundle-licenses \
