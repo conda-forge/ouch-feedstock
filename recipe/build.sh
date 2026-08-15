9cefe8f86adb7975c289d44595a5db7edea00cbc
Antônio Camargo <apcamargo@users.noreply.github.com>
2026-08-14 22:22:04 -0300

    Try to fix CXX flags

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,7 +7,14 @@ export CARGO_PROFILE_RELEASE_LTO=fat
 
 if [[ "${build_platform}" == linux-* ]]; then
     export LIBCLANG_PATH="$BUILD_PREFIX/lib"
-    export CXXFLAGS="${CXXFLAGS} -I${BUILD_PREFIX}/${BUILD}/include/c++/v1 -I${BUILD_PREFIX}/${BUILD}/sysroot/usr/include"
+
+    CXX_INCLUDE_DIR=$(find "${BUILD_PREFIX}/${HOST}/sysroot/usr/include/c++" -mindepth 1 -maxdepth 1 -type d | sort -V | tail -n1)
+    if [[ -z "${CXX_INCLUDE_DIR}" ]]; then
+        echo "ERROR: could not locate libstdc++ headers under ${BUILD_PREFIX}/${HOST}/sysroot/usr/include/c++" >&2
+        exit 1
+    fi
+
+    export CXXFLAGS="${CXXFLAGS} -I${CXX_INCLUDE_DIR} -I${CXX_INCLUDE_DIR}/${HOST} -I${BUILD_PREFIX}/${HOST}/sysroot/usr/include"
     export CXX="${CXX}"
 fi
 
