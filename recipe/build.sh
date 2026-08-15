cb91efabdc1dcf4e40ee65b620a8455936a7702b
Antônio Camargo <apcamargo@users.noreply.github.com>
2026-08-14 21:55:27 -0300

    Try `libstdcxx`

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,16 +7,6 @@ export CARGO_PROFILE_RELEASE_LTO=fat
 
 if [[ "${build_platform}" == linux-* ]]; then
     export LIBCLANG_PATH="$BUILD_PREFIX/lib"
-
-    export CC_x86_64_unknown_linux_gnu="${CC}"
-    export CXX_x86_64_unknown_linux_gnu="${CXX}"
-    export AR_x86_64_unknown_linux_gnu="${AR}"
-    export CXXFLAGS_x86_64_unknown_linux_gnu="${CXXFLAGS}"
-
-    export CC_aarch64_unknown_linux_gnu="${CC}"
-    export CXX_aarch64_unknown_linux_gnu="${CXX}"
-    export AR_aarch64_unknown_linux_gnu="${AR}"
-    export CXXFLAGS_aarch64_unknown_linux_gnu="${CXXFLAGS}"
 fi
 
 # Build binary with cargo-auditable
